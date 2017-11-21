1fe644ac09be3ddc052372b20502aa9a9fe82b26
yjchen5 <yjchen5@nuvoton.com>
2017-11-21 11:22:04 +0800

    1. Fix USBD driver Zero Flag issue. 2. Revise multi-function pins setting to avoid overwrite the previous correct settings (SPI, I2S and USBD).
    Change-Id: Ib4c601da94dd73da619e57fa95d34eff0663978c

--- SampleCode/StdDriver/USBD_MassStorage_DataFlash/main.c
+++ SampleCode/StdDriver/USBD_MassStorage_DataFlash/main.c
@@ -62,8 +62,10 @@ void SYS_Init(void)
     /*---------------------------------------------------------------------------------------------------------*/
 
     /* Set GPB multi-function pins for UART0 RXD and TXD, and Clock Output */
+    SYS->GPB_MFP &= ~(SYS_GPB_MFP_PB0_Msk | SYS_GPB_MFP_PB1_Msk | SYS_GPB_MFP_PB12_Msk);
     SYS->GPB_MFP |= (SYS_GPB_MFP_PB0_UART0_RXD | SYS_GPB_MFP_PB1_UART0_TXD | SYS_GPB_MFP_PB12_CLKO);
-    SYS->ALT_MFP |=  SYS_ALT_MFP_PB12_CLKO;
+    SYS->ALT_MFP &= ~SYS_ALT_MFP_PB12_Msk;
+    SYS->ALT_MFP |= SYS_ALT_MFP_PB12_CLKO;
 
     /* Enable CLKO(PB.12) for monitor HCLK. CLKO = HCLK/8 Hz*/
     EnableCLKO((2 << CLK_CLKSEL2_FRQDIV_S_Pos), 2);
